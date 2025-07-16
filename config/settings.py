7567935877dac7f2f464a199830f8ef7e9929c57
AhmadTawil1 <ahmadtawil.se@outlook.com>
2025-07-16 14:04:24 +0300

    for secure

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Now you can safely read variables
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
