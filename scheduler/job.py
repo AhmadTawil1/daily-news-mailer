ad7cf3f53412c40dec6f0cfac2ba9d9d5d6174d0
AhmadTawil1 <ahmadtawil.se@outlook.com>
2025-07-17 21:21:07 +0300

    A scheduler for sending everyday

---
+++ scheduler/job.py
@@ -0,0 +1,18 @@
+import schedule
+import time
+import subprocess
+import sys
+
+def run_daily_job():
+    print("Running daily news job...")
+    subprocess.run([sys.executable, "main.py"])
+
+# Schedule the job every day at 07:30 AM
+schedule.every().day.at("21:18").do(run_daily_job)
+
+print("✅ Scheduler started — waiting for 07:30...")
+
+# Keep the script running
+while True:
+    schedule.run_pending()
+    time.sleep(60)
