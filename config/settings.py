dd9ee0f639f54d0dbb5c53984cab4ec62c929891
AhmadTawil1 <ahmadtawil.se@outlook.com>
2025-07-17 21:20:21 +0300

    added email and password

--- config/settings.py
+++ config/settings.py
@@ -6,3 +6,5 @@ load_dotenv()
 
 # Now you can safely read variables
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
